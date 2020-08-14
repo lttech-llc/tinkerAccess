a8fd77b371b2b30f2fe820c52d84b11485140f0b
Little Thompson Technologies, LLC <todd.franks@littlethompsontechnologies.com>
2020-08-14 13:02:01 -0600

    Updated install script to support newer versions of pip

--- install.sh
+++ install.sh
@@ -7,29 +7,30 @@ fi
 
 dir="/opt/tinkeraccess"
 
-apt-get update
-apt-get install -U python-pip
-easy_install -U pip
-/usr/local/bin/pip install flask
+apt update
+apt install python-pip
+pip install --upgrade  setuptools pip
+pip install flask
 
 if [ ! -f /usr/bin/sqlite3 ] ; then
-apt-get update
-apt-get install sqlite3
+   apt update
+   apt install sqlite3
 fi
 
 # if the directory doesn't exist create it
 if [ ! -d $dir ] ; then
-		mkdir -p $dir
+   mkdir -p $dir
 fi
 
 # if the database doesn't exist create it
 if [ ! -f $dir/db.db ] ; then
-		sqlite3 $dir/db.db < schema.sql
+   sqlite3 $dir/db.db < schema.sql
 fi
 
 # copy over the file we need
 cp server.py $dir
-cp server.cfg $dir
+cp example_server.cfg $dir/server.cfg
+echo "Edit the ${dir}/server.cfg file before restarting"
 chmod 755 $dir/server.py
 cp -r static $dir
 cp -r templates $dir
@@ -42,4 +43,4 @@ update-rc.d tinkerserver defaults 91
 service tinkerserver restart
 
 # install the Client startup service
-pip install --upgrade --force-reinstall --ignore-installed --no-cache-dir tinker-access-client
+pip install --upgrade --force-reinstall --ignore-installed --no-cache-dir tinker-access-client --no-binary tinker-access-client

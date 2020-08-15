e52f7ecd78625e3e0bc0abff8d998a78cb8bfffa
Little Thompson Technologies, LLC <todd.franks@littlethompsontechnologies.com>
2020-08-14 21:58:16 -0600

    More install script updates

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ fi
 dir="/opt/tinkeraccess"
 
 apt update
-apt install python-pip
+apt install python-pip python-setuptools
 pip install flask
 
 if [ ! -f /usr/bin/sqlite3 ] ; then
@@ -26,10 +26,9 @@ if [ ! -f $dir/db.db ] ; then
    sqlite3 $dir/db.db < schema.sql
 fi
 
-# copy over the file we need
+# copy over the files we need
 cp server.py $dir
 cp example_server.cfg $dir/server.cfg
-echo "Edit the ${dir}/server.cfg file before restarting"
 chmod 755 $dir/server.py
 cp -r static $dir
 cp -r templates $dir
@@ -43,3 +42,9 @@ service tinkerserver restart
 
 # install the Client startup service
 pip install --upgrade --force-reinstall --ignore-installed --no-cache-dir tinker-access-client --no-binary tinker-access-client
+cp tinker_access_client/config_file/tinker-access-client.conf /etc
+
+echo ""
+echo "The ${dir}/server.cfg file needs edited for the server setup before restarting"
+echo "The /etc/tinker-access-client.conf file needs edited for the client setup before restarting"
+echo ""
